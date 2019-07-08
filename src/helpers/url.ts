5ebddcca09dd1a2d3702c35bcd165e6a7eaf893a
ShanaAE <Shana.wav@gmail.com>
2019-07-08 23:32:34 +0800

    fix(url parser): fix Date convertion error
    fix Date convertion error

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -32,7 +32,7 @@ export function buildURL(url: string, params?: any): string {
     }
     values.forEach(val => {
       if (isDate(val)) {
-        val = val.toISOString
+        val = val.toISOString()
       } else if (isObject(val)) {
         val = JSON.stringify(val)
       }
