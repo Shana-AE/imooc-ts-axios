deb1c637717fbdf134eb163ce9a2f050ffadba41
ShanaAE <Shana.wav@gmail.com>
2019-07-23 23:30:43 +0800

    chore(axios extend demo): add axios response data generic demo
    add axios response data generic demo

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -28,7 +28,7 @@ axios.post('/extend/post', { msg: 'post' })
 
 axios.put('/extend/put', { msg: 'put' })
 
-axios.patch('/extend/patch', { msg: 'patch' } )
+axios.patch('/extend/patch', { msg: 'patch' })
 
 axios({
   url: '/extend/post',
@@ -44,3 +44,29 @@ axios('/extend/post', {
     msg: 'hello'
   }
 })
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test().catch(err=> console.error(err))
